d9bb17500ce60612e644b71d3eb5d3a756dc16f2
Jan Hein Hoogstad <yeehaa@offcourse.io>
2024-08-21 17:32:49 +0200

    really happy with these refactors

--- packages/ui/tailwind.config.js
+++
@@ -1,6 +0,0 @@
-import sharedConfig from "@rizom/tailwind-config";
-const config = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    presets: [sharedConfig]
-};
-export default config;
