ec6be6de3209a39b89a1f2ef50950237f85cc1a2
Jan Hein Hoogstad <yeehaa@offcourse.io>
2024-09-07 16:50:33 +0200

    add options header

--- apps/yeehaa.io/src/pages/offcourse/query.json.ts
+++ apps/yeehaa.io/src/pages/offcourse/query.json.ts
@@ -4,6 +4,13 @@ import { handleQuery } from '@offcourse/db/query';
 import type { APIRoute } from 'astro';
 
 export const POST: APIRoute = async ({ request }) => {
+  if (request.method === "OPTIONS") {
+    return new Response("ok", {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      }
+    });
+  }
 
   if (request.headers.get("Content-Type") === "application/json") {
     const body = await request.json();
