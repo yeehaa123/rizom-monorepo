fc210a45ddd5580c8de3ab938d6993d9f42ca2f2
Jan Hein Hoogstad <yeehaa@offcourse.io>
2024-10-23 07:47:23 +0200

    wip: further card design (more)

--- packages/offcourse/ui/src/components/Tags.tsx
+++ packages/offcourse/ui/src/components/Tags.tsx
@@ -2,7 +2,7 @@ import { badgeVariants } from "./ui/badge"
 import { cn } from "@/lib/utils"
 
 export default function Tags({ tags }: { tags: string[] }) {
-  return <div className="flex flex-wrap gap-1.5">
+  return <div className="flex flex-wrap gap-1">
     {tags.map(tag => <a
       href={`/tags/${tag}`}
       className={cn(
