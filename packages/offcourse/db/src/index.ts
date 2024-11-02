495276f7418ebcdb30effa6cada01a439c0b717d
Jan Hein Hoogstad <yeehaa@offcourse.io>
2024-11-02 19:29:07 +0100

    very slight chance

--- packages/offcourse/db/src/index.ts
+++ packages/offcourse/db/src/index.ts
@@ -1,12 +1,11 @@
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
+import { config } from 'dotenv';
+config({ path: '.env' });
 
 const turso = createClient({
-  // @ts-ignore
-  url: process.env.TURSO_DATABASE_URL || import.meta.env.TURSO_DB_URL,
-  // @ts-ignore
-  authToken: process.env.TURSO_AUTH_TOKEN! || import.meta.env.TURSO_DB_AUTH_TOKEN,
-
+  url: process.env.TURSO_DATABASE_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN!,
 });
 
 
