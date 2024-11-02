38f5908150d93a56b0c433997b7f994b5b94a9d1
Jan Hein Hoogstad <yeehaa@offcourse.io>
2024-11-02 17:48:48 +0100

    wip: routes and collections

--- apps/offcourse.io/src/components/RoutesMenu.tsx
+++ apps/offcourse.io/src/components/RoutesMenu.tsx
@@ -1,14 +1,10 @@
 import { cn } from "@/lib/utils"
 import {
   NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
   navigationMenuTriggerStyle,
-  NavigationMenuViewport,
 } from "./ui/navigation-menu"
 
 type Props = {
@@ -22,7 +18,10 @@ export function RoutesMenu({ links, className, value }: Props) {
     <NavigationMenuList>
       <NavigationMenuItem>
         {links.sort((a, b) => a.title.localeCompare(b.title)).map(({ title, href }) =>
-          <NavigationMenuLink className={cn(navigationMenuTriggerStyle())} href={href}>{title}</NavigationMenuLink>)}
+          <NavigationMenuLink
+            active={title === value}
+            className={cn(navigationMenuTriggerStyle())}
+            href={href}>{title}</NavigationMenuLink>)}
       </NavigationMenuItem>
     </NavigationMenuList>
   </NavigationMenu>
