a556ee1378b93b004cba5287767eddac5c43f294
Jan Hein Hoogstad <yeehaa@offcourse.io>
2024-11-03 04:58:51 +0100

    try again

--- packages/offcourse/db/src/index.ts
+++ packages/offcourse/db/src/index.ts
@@ -4,7 +4,7 @@ import { config } from 'dotenv';
 config({ path: '.env' });
 
 const turso = createClient({
-  url: process.env.TURSO_DATABASE_URL!,
+  url: process.env.TURSO_DATABASE_URL! || process.env.PUBLIC_TURSO_DATABASE_URL!,
   authToken: process.env.TURSO_AUTH_TOKEN!,
 });
 
