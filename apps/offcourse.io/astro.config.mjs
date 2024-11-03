6525d06563f84bc47e39604dc4d5a300d0d0ffb3
Jan Hein Hoogstad <yeehaa@offcourse.io>
2024-11-03 18:48:56 +0100

    clean up

--- apps/offcourse.io/astro.config.mjs
+++ apps/offcourse.io/astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig, envField} from 'astro/config';
-import { addCMS } from "@rizom/cms";
 import { visit } from 'unist-util-visit';
 import section from '@hbsnow/rehype-sectionize';
 import classNames from 'rehype-class-names';
@@ -12,20 +11,7 @@ import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/serverless";
 import * as path from 'path';
-
 import offcourse from "@offcourse/astro"
-
-const OUTPUT_BASE = './src/content';
-const INPUT_BASE = './offcourse';
-const CONTENT_CONFIG = "./contentConfig.ts.template"
-const CMS_PATH = path.join("./CMS");
-
-const cmsConfig = {
-  input_base: INPUT_BASE, 
-  output_base: OUTPUT_BASE, 
-  cms_path: CMS_PATH,
-  content_config: CONTENT_CONFIG
-};
 
 export default defineConfig({
   site: 'https://offcourse.io',
@@ -40,7 +26,7 @@ export default defineConfig({
       'h1 + p': 'lead'
     }], slug, section]
   },
-  integrations: [addCMS(cmsConfig), tailwind({
+  integrations: [tailwind({
     applyBaseStyles: false
   }), offcourse(), mdx(), react(), sitemap()],
   server: { port: 8765},
