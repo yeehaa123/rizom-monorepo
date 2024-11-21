f2c37d0bf30f05e306c89d941df27dea7d16df31
Jan Hein Hoogstad <yeehaa@offcourse.io>
2024-11-21 14:20:25 +0100

    attempt three

--- packages/offcourse/astro/src/og.png.ts
+++ packages/offcourse/astro/src/og.png.ts
@@ -1,5 +1,4 @@
 import type { APIContext } from "astro";
-import { readFile } from "fs/promises";
 import type { Course } from "@offcourse/schema";
 import { CollectionType, QueryType } from "@offcourse/schema";
 import { handleQuery } from "@offcourse/db/query";
@@ -10,51 +9,52 @@ export async function getStaticPaths() {
     payload: CollectionType.enum.ALL
   }, false)
   const courses = (data?.payload || []) as Course[];
-  return courses.map((course) => {
+  const promises = courses.map(async (course) => {
+    const folderURL = "https://yeehaa.io/fonts";
+    const thin = await fetch(`${folderURL}/GT-Ultra-Standard-Thin.otf`);
+    const light = await fetch(`${folderURL}/GT-Ultra-Standard-Light.otf`);
+    const regular = await fetch(`${folderURL}/GT-Ultra-Standard-Regular.otf`);
+    const bold = await fetch(`${folderURL}/GT-Ultra-Standard-Bold.otf`);
+    const fonts = [
+      {
+        name: "GT Ultra Standard",
+        data: await thin.arrayBuffer(),
+        weight: 200,
+      },
+      {
+        name: "GT Ultra Standard",
+        data: await light.arrayBuffer(),
+        weight: 300,
+      },
+      {
+        name: "GT Ultra Standard",
+        data: await regular.arrayBuffer(),
+        weight: 500,
+      },
+      {
+        name: "GT Ultra Standard",
+        data: await bold.arrayBuffer(),
+        weight: 700,
+      }
+    ];
     const { courseId } = course;
+    const data = await handleQuery({
+      type: QueryType.enum.RENDER_COURSE_IMAGE,
+      payload: { course, fonts }
+    }, false)
+    const png = data.payload;
     return {
       params: { courseId },
       props: {
-        course
+        png
       }
     }
   })
+  return Promise.all(promises);
 }
 
 export async function GET({ props }: APIContext) {
-  const { course } = props;
-  const folderURL = "https://yeehaa.io/fonts";
-  const thin = await fetch(`${folderURL}/GT-Ultra-Standard-Thin.otf`);
-  const light = await fetch(`${folderURL}/GT-Ultra-Standard-Light.otf`);
-  const regular = await fetch(`${folderURL}/GT-Ultra-Standard-Regular.otf`);
-  const bold = await fetch(`${folderURL}/GT-Ultra-Standard-Bold.otf`);
-  const fonts = [
-    {
-      name: "GT Ultra Standard",
-      data: await thin.arrayBuffer(),
-      weight: 200,
-    },
-    {
-      name: "GT Ultra Standard",
-      data: await light.arrayBuffer(),
-      weight: 300,
-    },
-    {
-      name: "GT Ultra Standard",
-      data: await regular.arrayBuffer(),
-      weight: 500,
-    },
-    {
-      name: "GT Ultra Standard",
-      data: await bold.arrayBuffer(),
-      weight: 700,
-    }
-  ];
-  const data = await handleQuery({
-    type: QueryType.enum.RENDER_COURSE_IMAGE,
-    payload: { course, fonts }
-  }, false)
-  const png = data.payload
+  const { png } = props;
   // @ts-ignore
   return new Response(png, {
     headers: { 'Content-Type': 'image/png' },
